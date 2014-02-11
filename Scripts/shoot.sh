29e847bedecfa30652979e11d073782bf06bab4a
Chris Bayot <kobayashi@teamspooky.com>
2014-02-10 21:29:37 -0600

    remaking shoot.sh to make locally saved screnshot

--- Scripts/shoot.sh
+++ Scripts/shoot.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-function uploadImage {
-  curl -s -F "image=@$1" -F "key=486690f872c678126a2c09a9e196ce1b" https://imgur.com/api/upload.xml | grep -E -o "<original_image>(.)*</original_image>" | grep -E -o "http://i.imgur.com/[^<]*"
-}
-scrot -s "shot.png" 
-uploadImage "shot.png" | xclip -selection c
-rm shot.png
-notify-send "Done"
+import -window root ~/Pictures/Screenshots/$(date '+%Y%m%d-%H%M%S').png
