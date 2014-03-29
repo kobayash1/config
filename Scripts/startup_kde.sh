cbf2f92967c87519fe8e7838c7f3da6904edfd51
Chris Bayot <kobayashi@teamspooky.com>
2014-03-28 22:03:26 -0500

    Fixed bug where empty terminal not being sent to workspace 4

--- Scripts/startup_kde.sh
+++ Scripts/startup_kde.sh
@@ -2,10 +2,10 @@
 sleep 20 &&
 conky &
 konsole -e irssi &
+konsole &
 skype &
 sleep 30 &&
 (wmctrl -r "irssi" -t 2);(wmctrl -r "irssi" -b toggle,fullscreen)
+(wmctrl -r "bash" -t 3);(wmctrl -r "bash" -b toggle,fullscreen)
 sleep 40 &&
 wmctrl -r "skype" -t 1 &
-konsole &
-(wmctrl -r "bash" -t 3);(wmctrl -r "bash" -b toggle,fullscreen)
