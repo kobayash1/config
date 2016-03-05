5a1d58e3e5d10f77d2cd347f87d82bf7f8a5d65e
kobayash1 <c.bay87@gmail.com>
2016-03-04 19:55:02 -0600

    new stuff

--- Scripts/music.sh
+++
@@ -1,1 +0,0 @@
-mplayer -playlist <(find ~/Music -type f -iregex ".*\(adx\|flac\|mp3\|ogg\|wav\)" | sort -R)
--- Scripts/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ $XDG_CURRENT_DESKTOP = "KDE" ]
-then
-	source /home/kobayashi/Scripts/startup_kde.sh
-else
-	source /home/kobayashi/Scripts/startup_lxde.sh
-fi
